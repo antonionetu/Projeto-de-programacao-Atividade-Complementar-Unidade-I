d1aa89bd1668aa6832b6f93cd99b3f5c28aba91a
pedrocarvalho-13 <pedrogui.carvalho13@gmail.com>
2024-09-16 17:02:06 -0300

    Adição da classe AlunoPergamum

---
+++ biblioteca/AlunoPergamum.java
@@ -0,0 +1,69 @@
+package biblioteca;
+
+import java.util.ArrayList;
+
+public class AlunoPergamum {
+    private int matricula;
+    private String nome;
+    private ArrayList<LivroPergamum> emprestimos;
+    private int totalLivros;
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public ArrayList<LivroPergamum> getEmprestimos() {
+        return emprestimos;
+    }
+
+    public void setEmprestimos(ArrayList<LivroPergamum> emprestimos) {
+        this.emprestimos = emprestimos;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public int getTotalLivros() {
+        return totalLivros;
+    }
+
+    public AlunoPergamum() {
+
+    }
+
+    public AlunoPergamum(int Matricula, String Nome, int TotalLivros) {
+        this.matricula = Matricula;
+        this.nome = Nome;
+        this.totalLivros = TotalLivros;
+        this.emprestimos = new ArrayList<>();
+    }
+
+    public boolean EmprestarLivro(LivroPergamum livro) {
+        if (emprestimos.size() >= totalLivros) {
+            System.out.println("Limite de livros emoprestado atingido.");
+            return false;
+        } 
+        emprestimos.add(livro);
+        System.out.println("Livro emrpestado: " + livro.getTitulo());
+        return true;
+    }
+
+    public boolean DevolverLivro(LivroPergamum livro) {
+        for (int i = 0; i < emprestimos.size(); i++) {
+            if (emprestimos[i].getCodigo() == livro.getCodigo()) {
+                emprestimos.remove(i);
+                System.out.println("Livro devolvido.");
+                return true;
+            } 
+        }
+        System.out.println("Livro não enocntrado");
+        return false;
+    }
+
+}
